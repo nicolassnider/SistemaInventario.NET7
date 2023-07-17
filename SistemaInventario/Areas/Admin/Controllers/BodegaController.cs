ba910a0e4c12751aeb9377606027ffb52270a9fc
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-17 00:05:16 -0300

    video 33

---
+++ SistemaInventario/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BodegaController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var bodegas = await _unitOfWork.Bodega.GetAll();
+            return Json(new {data = bodegas});
+        }
+        #endregion
+    }
+}
---
+++ SistemaInventario/wwwroot/js/Bodega.js
@@ -0,0 +1,41 @@
+﻿let datatable;
+
+$(document).ready(function () {
+	loadDataTable();
+});
+
+function loadDataTable() {
+	datatable = $('#tblDatos').DataTable({
+		ajax: {
+			url: '/Admin/Bodega/GetAll',
+		},
+		columns: [
+			{ data: 'nombre', width: '20%' },
+			{ data: 'descripcion', width: '40%' },
+			/* { data: 'estado',
+				render: function (data) {
+					console.log(data);					
+					data == true ? 'Activo' : 'Inactivo';
+				},
+				width: '20%',
+			},
+			 */
+			{ data: 'estado', render:function(data){if(data){return "Activo"}else{return "Inactivo"}}, width: '40%' },
+			{ data: 'id',
+				render: function (data) {
+					return `
+						<div class="text-center">
+							<a href="/Admin/Bodega/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+								<i class="bi bi-pencil-square"></i>
+							</a>
+							<a onclick=Delete("/Admin/Bodega/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+								<i class="bi bi-trash"></i>
+							</a>
+						</div>
+					`;
+				},
+				width: '20%',
+			},
+		],
+	});
+}
