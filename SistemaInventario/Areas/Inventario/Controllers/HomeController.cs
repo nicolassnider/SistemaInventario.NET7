8e0cc9982e175c2b5afe5685236ca696916d345c
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-22 13:01:58 -0300

    diseño de página principal
    diseño de página principal

--- SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Models;
 using SistemaInventario.Models.ErrorViewModels;
 using System.Diagnostics;
 
@@ -8,15 +10,18 @@ namespace SistemaInventario.Areas.Inventario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<Producto> productoLista =await _unitOfWork.Producto.GetAll();
+            return View(productoLista);
         }
 
         public IActionResult Privacy()
