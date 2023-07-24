4645b331583bf85785a0b6dff972810b76d70972
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-23 21:38:00 -0300

    modificacion de appsettings.example
    implementacion de emailSernder y confirmación de email

--- SistemaInventario.Utilidades/EmailSender.cs
+++ SistemaInventario.Utilidades/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,21 @@ namespace SistemaInventario.Utilidades
 {
     public class EmailSender : IEmailSender
     {
+        private string SendGridSecret { get; set; }
+        private string EmailFrom { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+            EmailFrom = _config.GetValue<string>("SendGrid:EmailFrom");
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress(EmailFrom);
+            var to = new EmailAddress();
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return client.SendEmailAsync(msg);
         }
     }
 }
