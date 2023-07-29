24826be7f2824503b8c5d56568b5c2bdf359a429
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-29 20:51:02 -0300

    carroComprasVM metetodo detalle vista index

--- SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
 using SistemaInventario.Models;
 using SistemaInventario.Models.ErrorViewModels;
 using SistemaInventario.Models.Especificaciones;
+using SistemaInventario.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SistemaInventario.Areas.Inventario.Controllers
@@ -12,6 +13,8 @@ namespace SistemaInventario.Areas.Inventario.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public CarroComprasVM carroComprasVM { get; set; }
 
         public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
@@ -54,6 +57,31 @@ namespace SistemaInventario.Areas.Inventario.Controllers
             if (result.MetaData.TotalPages <= pageNumber) { ViewData["Siguiente"] = "disabled"; }
             return View(result);
 
+        }
+        public async Task<IActionResult> Detalle(int id)
+        {
+            carroComprasVM = new CarroComprasVM();
+            carroComprasVM.Compania =await _unitOfWork.Compania.GetFirstOrDefault();
+            carroComprasVM.Producto = await _unitOfWork.Producto
+                .GetFirstOrDefault(p => p.Id == id,
+                                   includeProperties:"Marca,Categoria");
+            var bodegaProducto = await _unitOfWork.BodegaProducto
+                .GetFirstOrDefault(b=>b.ProductoId==id && 
+                                   b.BodegaID==carroComprasVM.Compania.BodegaVentaId);
+            if (bodegaProducto != null)
+            {
+                carroComprasVM.Stock = 0;
+            }
+            else
+            {
+                carroComprasVM.Stock = bodegaProducto.Cantidad;
+            }
+            carroComprasVM.CarroCompras = new CarroCompras()
+            {
+                Producto = carroComprasVM.Producto,
+                ProductId = carroComprasVM.Producto.Id
+            };
+            return View(carroComprasVM);
         }
 
         public IActionResult Privacy()
