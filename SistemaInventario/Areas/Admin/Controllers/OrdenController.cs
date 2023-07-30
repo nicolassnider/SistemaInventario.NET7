e77239d2c0d53efdd284072f5a933df8ed7ad569
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-30 20:46:09 -0300

    ordencontroller

---
+++ SistemaInventario/Areas/Admin/Controllers/OrdenController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrdenController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrdenController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region
+        [HttpGet]
+        public async Task<IActionResult> ObtenerOrdenLista()
+        {
+            var todos = await _unitOfWork.Orden.GetAll(includeProperties:"UsuarioAplicacion");
+            return Json(new { data = todos });
+        }
+        #endregion
+    }
+}
