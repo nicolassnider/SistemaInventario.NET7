bb63a7be4a54b75a21986780de22600d0c7e8fc9
Nicolás Snider <nicolas.snider@outlook.com>
2023-07-30 21:26:10 -0300

    update de ObtenerOrdenLista

--- SistemaInventario/Areas/Admin/Controllers/OrdenController.cs
+++ SistemaInventario/Areas/Admin/Controllers/OrdenController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Models;
+using SistemaInventario.Utilidades;
+using System.Security.Claims;
 
 namespace SistemaInventario.Areas.Admin.Controllers
 {
@@ -14,15 +17,46 @@ namespace SistemaInventario.Areas.Admin.Controllers
             _unitOfWork = unitOfWork;
 
         }
+        
         public IActionResult Index()
         {
             return View();
         }
+
+        private Claim obtenerUsuarioId()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            return claim;
+        }
         #region
         [HttpGet]
-        public async Task<IActionResult> ObtenerOrdenLista()
+        public async Task<IActionResult> ObtenerOrdenLista(string estado)
         {
-            var todos = await _unitOfWork.Orden.GetAll(includeProperties:"UsuarioAplicacion");
+            var claim = obtenerUsuarioId();
+            IEnumerable<Orden> todos;
+            if (User.IsInRole(DS.Role_Admin))//validar rol del us
+            {
+                todos = await _unitOfWork.Orden
+                    .GetAll(includeProperties: "UsuarioAplicacion");
+            }else
+            {
+                todos = await _unitOfWork.Orden
+                    .GetAll(o=>o.UsuarioAplicacionId==claim.Value, includeProperties: "UsuarioAplicacion");
+            }
+
+            switch (estado)
+            {
+                case "aprobado":
+                    todos = todos.Where(o => o.EstadoOrden == DS.EstadoAprobado);
+                    break;
+                case "completado":
+                    todos = todos.Where(o => o.EstadoOrden == DS.EstadoEnviado);
+                    break;
+                default:
+                    break;
+
+            }
             return Json(new { data = todos });
         }
         #endregion
